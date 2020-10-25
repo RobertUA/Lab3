ab7100061279609cbe9e01834923ba923a0b9301
Роберт Шевцов <55320224+RobertUA@users.noreply.github.com>
2020-10-25 15:34:20 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
+
 function hashchanged()
 {
-    if(window.location.hash.slice(1)=="contacts")
+    if(window.location.hash=="#contacts")
     {
-        console.log("!");
-        
+        document.getElementById("cnt").classList.add("active");
+        document.getElementById("ctlg").classList.remove("active");
+
+        document.getElementById("main").style.display="none";
+        document.getElementById("main2").style.display="grid";
+    }
+    else if(window.location.hash=="#catalog")
+    {
+        document.getElementById("ctlg").classList.add("active");
+        document.getElementById("cnt").classList.remove("active");
+
+        document.getElementById("main2").style.display="none";
+        document.getElementById("main").style.display="flex";
+
     }
 }
